58d47cdb3ce20e80b518263af5175d0b55c8bdfa
SamuraiDevCo <160781327+SamuraiDevCo@users.noreply.github.com>
2024-07-03 09:01:44 -0400

    Escrow Fixes

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,29 +2,27 @@ fx_version 'cerulean'
 games { 'gta5' }
 
 author 'HoboDevCo#3011'
-description 'SDC | Object Placer Script'
+description 'SDC | Metal Detector Script'
 version '1.0.0'
 
 shared_script {
-    "config/config.lua",
-    "config/lang.lua"
+    "config/config.lua"
 }
 
 client_scripts {
-    "src/client/client_customize_me.lua",
-    'src/client/client.lua'
+    "src/client/client.lua"
 }
 
 server_scripts {
-    'src/server/server.lua',
+    "src/server/server_customize_me.lua",
+    "src/server/server.lua",
 }
 
 escrow_ignore {
     "config/config.lua",
-    "config/lang.lua",
-    "src/client/client_customize_me.lua",
-    'src/client/client.lua',
-    'src/server/server.lua',
+    "src/client/client.lua",
+    "src/server/server.lua",
+    "src/server/server_customize_me.lua",
 }
 
 lua54 'yes'
